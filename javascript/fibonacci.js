be5d4949a91a45599bf13fb35b43c6fd08692e12
Karen Olson <karmarie@gmail.com>
2021-12-15 17:27:16 -0600

    Create fibonacci function in Ruby and Javascript

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,30 @@
 function fibonacci(num) {
-  // type your code here
+  let arr = [0, 1];
+  let i = 2;
+
+  while (i <= num) {
+    newNumber = arr[0] + arr[1];
+    arr[0] = arr[1];
+    arr[1] = newNumber;
+    i++;
+  }
+
+  return arr[1];
 }
+
+// Overarching strategy:
+//    use an array to hold placeholder values for 2 numbers
+//    sum the numbers, then replace the values so that you're tracking the most recent sum and the previous value
+//    keep following this pattern until you reach the desired number
+// Steps:
+// create an array to hold two values. initially set them to 0 and 1
+// iterate through the array num-1 times (or while the iterator is less than num)
+// start the iterator at 2 because this is the "starting index" (you've already hard-coded the first 2 numbers in the sequence)
+// calculate the sum of the values in arr
+// move the second arr value to first space
+// put the newNumber in the second space
+// increment the iterator
+// return the most recent newNumber
 
 if (require.main === module) {
   // add your own tests in here
--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,6 +1,44 @@
+require 'pry'
+
+# def fibonacci(num)
+#   arr = [0, 1]
+#   i = 2
+# 
+#   while i <= num
+#     new_number = arr[i-2] + arr[i-1]
+#     arr.push(new_number)
+#     i+=1
+#   end
+# 
+#   arr.last
+# end 
+
+#   # create an empty array to hold the fibonacci sequence. manually enter the first 2 numbers bc they don't follow the pattern
+#   # start the iterator at 2 because that's the index you're starting at
+#     # get the next number by summing the previous two numbers
+#     # add the new number to the end of the array
+#     # increment the iterator
+#   # return the last number in the array
+
 def fibonacci(num)
-  # type your code in here
+  arr = [0, 1]
+  
+  (num-1).times do
+    new_number = arr[0] + arr[1]
+    arr[0] = arr[1]
+    arr[1] = new_number
+  end
+  
+  arr[1]
 end
+
+# create an array to hold two values. initially set them to 0 and 1 because these numbers start the sequence
+# loop through the array num-1 times
+# calculate the new number using the two existing numbers in the array
+# put the value of the second number in place of the first
+# put the new number in place of the second
+# keep going until you've looped through num-1 times because of zero indexing
+# return the most recent "new number" (sum of the previous two)
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 0"
@@ -18,6 +56,5 @@ if __FILE__ == $PROGRAM_NAME
 
   # Don't forget to add your own!
 end
-
 # Please add your pseudocode to this file
 # And a written explanation of your solution
